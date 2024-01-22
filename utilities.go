c7be2acf765750da6820f5f772f3266b33dc991f
Conero007 <rohan.jayesh18@gmail.com>
2024-01-22 11:07:56 +0530

    [added] utilities for url shortening

---
+++ utilities.go
@@ -0,0 +1,14 @@
+package main
+
+import "math/rand"
+
+const keyLength = 6
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateShortKey() string {
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
