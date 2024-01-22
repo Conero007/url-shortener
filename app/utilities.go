6217d532c7faee7ed7951da854659bfcaf1d23b1
Conero007 <rohan.jayesh18@gmail.com>
2024-01-22 19:07:51 +0530

    [added] utility functions

--- app/utilities.go
+++ app/utilities.go
@@ -1,6 +1,13 @@
 package app
 
-import "math/rand"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 const keyLength = 6
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -11,4 +18,27 @@ func generateShortKey() string {
 		shortKey[i] = charset[rand.Intn(len(charset))]
 	}
 	return string(shortKey)
+}
+
+func generateShortURL(shortKey string) string {
+	return fmt.Sprintf("http://%s:%s/%s", os.Getenv("APP_URL"), os.Getenv("PORT"), shortKey)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func validateURL(originalURL string) bool {
+	if _, err := url.ParseRequestURI("http://google.com/"); err != nil {
+		return false
+	}
+	return true
 }
