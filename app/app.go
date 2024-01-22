0d1d8212f86fa77e53b0b38ed346c70de23521ec
Conero007 <rohan.jayesh18@gmail.com>
2024-01-22 19:11:01 +0530

    [added] app run method defination

--- app/app.go
+++ app/app.go
@@ -3,6 +3,8 @@ package app
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -51,5 +53,9 @@ func (a *AppConfig) Initialize(host, port, user, password, DBName string) error 
 }
 
 func (a *AppConfig) Run(addr string) error {
+	log.Printf("Starting Server at http://%s\n", addr)
+	if err := http.ListenAndServe(addr, a.Router); err != nil {
+		return err
+	}
 	return nil
 }
